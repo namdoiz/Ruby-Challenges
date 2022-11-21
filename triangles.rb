efe3386434ea790d2289d05f9b7347f7b8ec8c88
namdoiz <nnamdi.on7@gmail.com>
2022-11-21 02:16:51 -0600

    made the validate_sides method private

--- triangles.rb
+++ triangles.rb
@@ -6,15 +6,17 @@ class Triangle
     validate_sides
   end
 
-  def validate_sides
-    raise (ArgumentError) if [@s1, @s2, @s3].any?{|side| side <= 0}
-    raise (ArgumentError) if [[[@s1, @s2], @s3], [[@s1, @s3], @s2], [[@s2, @s3], @s1]].any?{|sides| sides[0].sum <= sides[1]}
-  end
-
   def kind
     sides = [@s1, @s2, @s3]
     return 'equilateral' if sides.all?{|side| side == sides[0]}
     return 'isosceles' if [[@s1, @s2], [@s1, @s3], [@s2, @s3]].any?{|sides| sides[0] == sides[1]}
     'scalene'
+  end
+
+  private
+
+  def validate_sides
+    raise (ArgumentError) if [@s1, @s2, @s3].any?{|side| side <= 0}
+    raise (ArgumentError) if [[[@s1, @s2], @s3], [[@s1, @s3], @s2], [[@s2, @s3], @s1]].any?{|sides| sides[0].sum <= sides[1]}
   end
 end
