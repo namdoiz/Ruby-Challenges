fb350634a256e8866cc96f8bd1be0907daa3ce72
namdoiz <nnamdi.on7@gmail.com>
2022-12-02 02:24:45 -0600

    test suite for the robot name program

---
+++ robot_name_test.rb
@@ -0,0 +1,55 @@
+require 'minitest/autorun'
+require_relative 'robot_name'
+require 'minitest/reporters'
+
+Minitest::Reporters.use!
+
+class RobotTest < Minitest::Test
+  DIFFERENT_ROBOT_NAME_SEED = 1234
+  SAME_INITIAL_ROBOT_NAME_SEED = 1000
+
+  NAME_REGEXP = /^[A-Z]{2}\d{3}$/
+
+  def test_has_name
+    assert_match NAME_REGEXP, Robot.new.name
+  end
+
+  def test_name_sticks
+    #skip
+    robot = Robot.new
+    robot.name
+    assert_equal robot.name, robot.name
+  end
+
+  def test_different_robots_have_different_names
+    #skip
+    Kernel.srand DIFFERENT_ROBOT_NAME_SEED
+    refute_equal Robot.new.name, Robot.new.name
+  end
+
+  def test_reset_name
+    #skip
+    Kernel.srand DIFFERENT_ROBOT_NAME_SEED
+    robot = Robot.new
+    name = robot.name
+    robot.reset
+    name2 = robot.name
+    refute_equal name, name2
+    assert_match NAME_REGEXP, name2
+  end
+
+  def test_different_name_when_chosen_name_is_taken
+    #skip
+    Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
+    name1 = Robot.new.name
+    Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
+    name2 = Robot.new.name
+    refute_equal name1, name2
+  end
+end
+
+=begin
+  - calling Kernel.srand with the initial number sets the name to one thing
+  - calliing Kernel.srand with the same number sets the name to the same thing
+  - both names are going to be equal
+=end
